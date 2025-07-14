9682307a5835ea57c0d70b26d47c742727cd4c6f
Daniel Perez <dspwebstudio@gmail.com>
2025-07-14 15:06:05 -0600

    UI: Actualizar los colores de éxito en globals.css y mejorar el componente ColorDemo con nuevos estilos y elementos de color

--- src/components/ColorDemo.tsx
+++ src/components/ColorDemo.tsx
@@ -7,32 +7,44 @@ export default function ColorDemo() {
 	return (
 		<div className="flex flex-col gap-6 p-8 bg-neutral">
 			<H2 align="center">Colores Tema</H2>
-			<div className="flex gap-4 flex-wrap mt-20">
-				<div className="w-30 h-20 rounded-full bg-primary text-white flex items-center justify-center shadow">
-					primary
-				</div>
-				<div className="w-30 h-20 rounded-full bg-primary-light text-primary-dark flex items-center justify-center shadow">
-					primary-light
-				</div>
-				<div className="w-30 h-20 rounded-full bg-primary-dark text-white flex items-center justify-center shadow">
-					primary-dark
-				</div>
-				<div className="w-30 h-20 rounded-full bg-secondary text-white flex items-center justify-center shadow">
-					secondary
-				</div>
-				<div className="w-30 h-20 rounded-full bg-secondary-light text-secondary-dark flex items-center justify-center shadow">
-					secondary-light
-				</div>
-				<div className="w-30 h-20 rounded-full bg-secondary-dark text-white flex items-center justify-center shadow">
-					secondary-dark
-				</div>
-				<div className="w-30 h-20 rounded-full bg-accent text-white flex items-center justify-center shadow">
-					accent
-				</div>
-				<div className="w-30 h-20 rounded-full bg-neutral text-primary flex items-center justify-center shadow border border-gray-300">
-					neutral
-				</div>
-			</div>
+			<ul className="flex gap-4 flex-wrap mt-20 uppercase">
+				<li className="w-20 h-20 rounded-full bg-primary text-white flex items-center justify-center shadow text-2xl">
+					P
+				</li>
+				<li className="w-20 h-20 rounded-full bg-primary-light text-primary-dark flex items-center justify-center shadow text-2xl">
+					PL
+				</li>
+				<li className="w-20 h-20 rounded-full bg-primary-dark text-white flex items-center justify-center shadow text-2xl">
+					PD
+				</li>
+				<li className="w-20 h-20 rounded-full bg-secondary text-white flex items-center justify-center shadow text-2xl">
+					S
+				</li>
+				<li className="w-20 h-20 rounded-full bg-secondary-light text-secondary-dark flex items-center justify-center shadow text-2xl">
+					SL
+				</li>
+				<li className="w-20 h-20 rounded-full bg-secondary-dark text-white flex items-center justify-center shadow text-2xl">
+					SD
+				</li>
+				<li className="w-20 h-20 rounded-full bg-accent text-white flex items-center justify-center shadow text-2xl">
+					A
+				</li>
+				<li className="w-20 h-20 rounded-full bg-warning text-white flex items-center justify-center shadow text-2xl">
+					W
+				</li>
+				<li className="w-20 h-20 rounded-full bg-danger text-white flex items-center justify-center shadow text-2xl">
+					D
+				</li>
+				<li className="w-20 h-20 rounded-full bg-success text-success-dark flex items-center justify-center shadow text-2xl border border-success-dark">
+					S
+				</li>
+				<li className="w-20 h-20 rounded-full bg-dark text-white flex items-center justify-center shadow text-2xl border border-gray-300">
+					DK
+				</li>
+				<li className="w-20 h-20 rounded-full bg-neutral text-primary flex items-center justify-center shadow border border-gray-300 text-2xl">
+					N
+				</li>
+			</ul>
 		</div>
 	);
 }
