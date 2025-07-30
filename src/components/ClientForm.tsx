1b55679d04ec26ee72b9e801a6b6b64fa2e991ab
Daniel Perez <dspwebstudio@gmail.com>
2025-07-29 19:14:14 -0600

    UI: Aumentar el margen en secciones del formulario de cliente y centrar los títulos para mejorar la presentación

--- src/components/ClientForm.tsx
+++ src/components/ClientForm.tsx
@@ -321,7 +321,7 @@ const ClientForm = React.forwardRef(function ClientForm(
 			<div className="h-2"></div>
 			<form ref={formRef} className="p-8" onSubmit={handleSubmit}>
 				{/* DATOS PERSONALES */}
-				<div className="mb-12">
+				<div className="mb-24">
 					<H4 align="center">Datos personales</H4>
 					<div className="grid grid-cols-1 md:grid-cols-5 gap-6 mt-12">
 						<div className="flex flex-col items-center gap-4">
@@ -412,9 +412,160 @@ const ClientForm = React.forwardRef(function ClientForm(
 					</div>
 				</div>
 
-				{/* UBICACIÓN */}
-				<div className="mb-12">
-					<H4 align="left">Ubicación</H4>
+				{/* REDES SOCIALES */}
+				<div className="mb-24">
+					<H4 align="center">Redes sociales</H4>
+					<div className="grid grid-cols-1 md:grid-cols-4 gap-6 mt-8">
+						<div className="flex flex-col items-center gap-4">
+							<div className="flex items-center gap-2 justify-start w-full">
+								<FaFacebook className="text-primary-dark text-2xl" />
+								<label className="text-base">Facebook</label>
+							</div>
+							<input
+								name="facebook"
+								value={form.socialMedia.facebook}
+								onChange={(e) => handleNestedChange(e, "socialMedia")}
+								className={`pl-4 input w-full rounded-2xl border-2 border-blue-800 py-2 ${
+									form.socialMedia.facebook
+										? "bg-blue-50 text-primary-dark dark:bg-gray-700 dark:text-gray-300"
+										: ""
+								}`}
+							/>
+						</div>
+						<div className="flex flex-col items-center gap-4">
+							<div className="flex items-center gap-2 justify-start w-full">
+								<FaInstagram className="text-primary-dark text-2xl" />
+								<label className="text-base">Instagram</label>
+							</div>
+							<input
+								name="instagram"
+								value={form.socialMedia.instagram}
+								onChange={(e) => handleNestedChange(e, "socialMedia")}
+								className={`pl-4 input w-full rounded-2xl border-2 border-blue-800 py-2 ${
+									form.socialMedia.instagram
+										? "bg-blue-50 text-primary-dark dark:bg-gray-700 dark:text-gray-300"
+										: ""
+								}`}
+							/>
+						</div>
+						<div className="flex flex-col items-center gap-4">
+							<div className="flex items-center gap-2 justify-start w-full">
+								<FaLinkedin className="text-primary-dark text-2xl" />
+								<label className="text-base">LinkedIn</label>
+							</div>
+							<input
+								name="linkedin"
+								value={form.socialMedia.linkedin}
+								onChange={(e) => handleNestedChange(e, "socialMedia")}
+								className={`pl-4 input w-full rounded-2xl border-2 border-blue-800 py-2 ${
+									form.socialMedia.linkedin
+										? "bg-blue-50 text-primary-dark dark:bg-gray-700 dark:text-gray-300"
+										: ""
+								}`}
+							/>
+						</div>
+						<div className="flex flex-col items-center gap-4">
+							<div className="flex items-center gap-2 justify-start w-full">
+								<FaUser className="text-primary-dark text-2xl" />
+								<label className="text-base">X (Twitter)</label>
+							</div>
+							<input
+								name="x"
+								value={form.socialMedia.x}
+								onChange={(e) => handleNestedChange(e, "socialMedia")}
+								className={`pl-4 input w-full rounded-2xl border-2 border-blue-800 py-2 ${
+									form.socialMedia.x
+										? "bg-blue-50 text-primary-dark dark:bg-gray-700 dark:text-gray-300"
+										: ""
+								}`}
+							/>
+						</div>
+					</div>
+				</div>
+
+				{/* EMPRESA */}
+				<div className="mb-24">
+					<H4 align="center">Empresa</H4>
+					<div className="grid grid-cols-1 md:grid-cols-4 gap-6 mt-8">
+						<div className="flex flex-col items-center gap-4">
+							<div className="flex items-center gap-2 justify-start w-full">
+								<FaBuilding className="text-primary-dark text-2xl" />
+								<label className="text-base">Empresa</label>
+							</div>
+							<input
+								required
+								name="company"
+								value={form.company}
+								onChange={handleChange}
+								className={`pl-4 input w-full rounded-2xl border-2 border-blue-800 py-2 ${
+									form.company
+										? "bg-blue-50 text-primary-dark dark:bg-gray-700 dark:text-gray-300"
+										: ""
+								}`}
+							/>
+						</div>
+						<div className="flex flex-col items-center gap-4">
+							<div className="flex items-center gap-2 justify-start w-full">
+								<FaGlobe className="text-primary-dark text-2xl" />
+								<label className="text-base">Sitio Web</label>
+							</div>
+							<input
+								required
+								name="website"
+								value={form.website}
+								onChange={handleChange}
+								className={`pl-4 input w-full rounded-2xl border-2 border-blue-800 py-2 ${
+									form.website
+										? "bg-blue-50 text-primary-dark dark:bg-gray-700 dark:text-gray-300"
+										: ""
+								}`}
+							/>
+						</div>
+						<div className="flex flex-col items-center gap-4">
+							<div className="flex items-center gap-2 justify-start w-full">
+								<FaEnvelope className="text-primary-dark text-2xl" />
+								<label className="text-base">Email</label>
+							</div>
+							<input
+								required
+								name="email"
+								value={form.email}
+								onChange={handleChange}
+								className={`pl-4 input w-full rounded-2xl border-2 border-blue-800 py-2 ${
+									form.email
+										? "bg-blue-50 text-primary-dark dark:bg-gray-700 dark:text-gray-300"
+										: ""
+								}`}
+							/>
+						</div>
+						<div className="flex flex-col items-center gap-4">
+							<div className="flex items-center gap-2 justify-start w-full">
+								<FaIndustry className="text-primary-dark text-2xl" />
+								<label className="text-base">Industria</label>
+							</div>
+							<select
+								name="industry"
+								value={form.industry}
+								onChange={handleSelectChange}
+								className="pl-4 input w-full rounded-2xl border-2 border-blue-800 py-2 dark:bg-gray-700 dark:text-gray-300">
+								<option value="">Selecciona una industria</option>
+								<option value="Tecnología">Tecnología</option>
+								<option value="Salud">Salud</option>
+								<option value="Educación">Educación</option>
+								<option value="Finanzas">Finanzas</option>
+								<option value="Comercio">Comercio</option>
+								<option value="Manufactura">Manufactura</option>
+								<option value="Construcción">Construcción</option>
+								<option value="Alimentos y Bebidas">Alimentos y Bebidas</option>
+								<option value="Transporte y Logística">
+									Transporte y Logística
+								</option>
+								<option value="Turismo">Turismo</option>
+								<option value="Servicios">Servicios</option>
+								<option value="Otra">Otra</option>
+							</select>
+						</div>
+					</div>
 					<div className="grid grid-cols-1 md:grid-cols-4 gap-6 mt-8">
 						<div className="flex flex-col items-center gap-4">
 							<div className="flex items-center gap-2 justify-start w-full">
@@ -515,93 +666,21 @@ const ClientForm = React.forwardRef(function ClientForm(
 					</div>
 				</div>
 
-				{/* REDES SOCIALES */}
-				<div className="mb-12">
-					<H4 align="left">Redes sociales</H4>
-					<div className="grid grid-cols-1 md:grid-cols-4 gap-6 mt-8">
+				{/* OTROS CAMPOS */}
+				<div className="mb-24">
+					<H4 align="center">Otros</H4>
+					<div className="grid grid-cols-1 md:grid-cols-4 gap-12 mt-12">
 						<div className="flex flex-col items-center gap-4">
 							<div className="flex items-center gap-2 justify-start w-full">
-								<FaFacebook className="text-primary-dark text-2xl" />
-								<label className="text-base">Facebook</label>
+								<FaFileSignature className="text-primary-dark text-2xl" />
+								<label className="text-base">Logo URL</label>
 							</div>
 							<input
-								name="facebook"
-								value={form.socialMedia.facebook}
-								onChange={(e) => handleNestedChange(e, "socialMedia")}
-								className={`pl-4 input w-full rounded-2xl border-2 border-blue-800 py-2 ${
-									form.socialMedia.facebook
-										? "bg-blue-50 text-primary-dark dark:bg-gray-700 dark:text-gray-300"
-										: ""
-								}`}
-							/>
-						</div>
-						<div className="flex flex-col items-center gap-4">
-							<div className="flex items-center gap-2 justify-start w-full">
-								<FaInstagram className="text-primary-dark text-2xl" />
-								<label className="text-base">Instagram</label>
-							</div>
-							<input
-								name="instagram"
-								value={form.socialMedia.instagram}
-								onChange={(e) => handleNestedChange(e, "socialMedia")}
-								className={`pl-4 input w-full rounded-2xl border-2 border-blue-800 py-2 ${
-									form.socialMedia.instagram
-										? "bg-blue-50 text-primary-dark dark:bg-gray-700 dark:text-gray-300"
-										: ""
-								}`}
-							/>
-						</div>
-						<div className="flex flex-col items-center gap-4">
-							<div className="flex items-center gap-2 justify-start w-full">
-								<FaLinkedin className="text-primary-dark text-2xl" />
-								<label className="text-base">LinkedIn</label>
-							</div>
-							<input
-								name="linkedin"
-								value={form.socialMedia.linkedin}
-								onChange={(e) => handleNestedChange(e, "socialMedia")}
-								className={`pl-4 input w-full rounded-2xl border-2 border-blue-800 py-2 ${
-									form.socialMedia.linkedin
-										? "bg-blue-50 text-primary-dark dark:bg-gray-700 dark:text-gray-300"
-										: ""
-								}`}
-							/>
-						</div>
-						<div className="flex flex-col items-center gap-4">
-							<div className="flex items-center gap-2 justify-start w-full">
-								<FaUser className="text-primary-dark text-2xl" />
-								<label className="text-base">X (Twitter)</label>
-							</div>
-							<input
-								name="x"
-								value={form.socialMedia.x}
-								onChange={(e) => handleNestedChange(e, "socialMedia")}
-								className={`pl-4 input w-full rounded-2xl border-2 border-blue-800 py-2 ${
-									form.socialMedia.x
-										? "bg-blue-50 text-primary-dark dark:bg-gray-700 dark:text-gray-300"
-										: ""
-								}`}
-							/>
-						</div>
-					</div>
-				</div>
-
-				{/* EMPRESA */}
-				<div className="mb-12">
-					<H4 align="left">Empresa</H4>
-					<div className="grid grid-cols-1 md:grid-cols-4 gap-6 mt-8">
-						<div className="flex flex-col items-center gap-4">
-							<div className="flex items-center gap-2 justify-start w-full">
-								<FaBuilding className="text-primary-dark text-2xl" />
-								<label className="text-base">Empresa</label>
-							</div>
-							<input
-								required
-								name="company"
-								value={form.company}
+								name="logoUrl"
+								value={form.logoUrl}
 								onChange={handleChange}
 								className={`pl-4 input w-full rounded-2xl border-2 border-blue-800 py-2 ${
-									form.company
+									form.logoUrl
 										? "bg-blue-50 text-primary-dark dark:bg-gray-700 dark:text-gray-300"
 										: ""
 								}`}
@@ -609,132 +688,55 @@ const ClientForm = React.forwardRef(function ClientForm(
 						</div>
 						<div className="flex flex-col items-center gap-4">
 							<div className="flex items-center gap-2 justify-start w-full">
-								<FaGlobe className="text-primary-dark text-2xl" />
-								<label className="text-base">Sitio Web</label>
-							</div>
-							<input
-								required
-								name="website"
-								value={form.website}
-								onChange={handleChange}
-								className={`pl-4 input w-full rounded-2xl border-2 border-blue-800 py-2 ${
-									form.website
-										? "bg-blue-50 text-primary-dark dark:bg-gray-700 dark:text-gray-300"
-										: ""
-								}`}
-							/>
-						</div>
-						<div className="flex flex-col items-center gap-4">
-							<div className="flex items-center gap-2 justify-start w-full">
-								<FaEnvelope className="text-primary-dark text-2xl" />
-								<label className="text-base">Email</label>
-							</div>
-							<input
-								required
-								name="email"
-								value={form.email}
-								onChange={handleChange}
-								className={`pl-4 input w-full rounded-2xl border-2 border-blue-800 py-2 ${
-									form.email
-										? "bg-blue-50 text-primary-dark dark:bg-gray-700 dark:text-gray-300"
-										: ""
-								}`}
-							/>
-						</div>
-						<div className="flex flex-col items-center gap-4">
-							<div className="flex items-center gap-2 justify-start w-full">
-								<FaIndustry className="text-primary-dark text-2xl" />
-								<label className="text-base">Industria</label>
+								<FaFileSignature className="text-primary-dark text-2xl" />
+								<label className="text-base">Condiciones de pago</label>
 							</div>
 							<select
-								name="industry"
-								value={form.industry}
+								required
+								name="paymentTerms"
+								value={form.paymentTerms}
 								onChange={handleSelectChange}
 								className="pl-4 input w-full rounded-2xl border-2 border-blue-800 py-2 dark:bg-gray-700 dark:text-gray-300">
-								<option value="">Selecciona una industria</option>
-								<option value="Tecnología">Tecnología</option>
-								<option value="Salud">Salud</option>
-								<option value="Educación">Educación</option>
-								<option value="Finanzas">Finanzas</option>
-								<option value="Comercio">Comercio</option>
-								<option value="Manufactura">Manufactura</option>
-								<option value="Construcción">Construcción</option>
-								<option value="Alimentos y Bebidas">Alimentos y Bebidas</option>
-								<option value="Transporte y Logística">
-									Transporte y Logística
+								<option value="">Selecciona una opción</option>
+								<option value="Contado">Contado</option>
+								<option value="7 días">7 días</option>
+								<option value="15 días">15 días</option>
+								<option value="30 días">30 días</option>
+								<option value="60 días">60 días</option>
+								<option value="90 días">90 días</option>
+								<option value="Anticipo 50% y 50% a la entrega">
+									Anticipo 50% y 50% a la entrega
 								</option>
-								<option value="Turismo">Turismo</option>
-								<option value="Servicios">Servicios</option>
 								<option value="Otra">Otra</option>
 							</select>
 						</div>
+						<div className="flex flex-col items-center gap-4 md:col-span-2">
+							<div className="flex items-center gap-2 justify-start w-full">
+								<FaFileSignature className="text-primary-dark text-2xl" />
+								<label className="text-base">Notas</label>
+							</div>
+							<textarea
+								name="notes"
+								value={form.notes}
+								onChange={(e) => setForm({ ...form, notes: e.target.value })}
+								rows={8}
+								className={`p-6 input w-full rounded-2xl border-2 border-blue-800 ${
+									form.notes
+										? "bg-blue-50 text-primary-dark dark:bg-gray-700 dark:text-gray-300"
+										: ""
+								}`}
+							/>
+						</div>
 					</div>
 				</div>
 
-				{/* OTROS CAMPOS */}
-				<H4 align="left">Otros</H4>
-				<div className="grid grid-cols-1 md:grid-cols-4 gap-12">
-					<div className="flex flex-col items-center gap-4">
-						<div className="flex items-center gap-2 justify-start w-full">
-							<FaFileSignature className="text-primary-dark text-2xl" />
-							<label className="text-base">Logo URL</label>
-						</div>
-						<input
-							name="logoUrl"
-							value={form.logoUrl}
-							onChange={handleChange}
-							className={`pl-4 input w-full rounded-2xl border-2 border-blue-800 py-2 ${
-								form.logoUrl
-									? "bg-blue-50 text-primary-dark dark:bg-gray-700 dark:text-gray-300"
-									: ""
-							}`}
-						/>
-					</div>
-					<div className="flex flex-col items-center gap-4">
-						<div className="flex items-center gap-2 justify-start w-full">
-							<FaFileSignature className="text-primary-dark text-2xl" />
-							<label className="text-base">Condiciones de pago</label>
-						</div>
-						<select
-							required
-							name="paymentTerms"
-							value={form.paymentTerms}
-							onChange={handleSelectChange}
-							className="pl-4 input w-full rounded-2xl border-2 border-blue-800 py-2 dark:bg-gray-700 dark:text-gray-300">
-							<option value="">Selecciona una opción</option>
-							<option value="Contado">Contado</option>
-							<option value="7 días">7 días</option>
-							<option value="15 días">15 días</option>
-							<option value="30 días">30 días</option>
-							<option value="60 días">60 días</option>
-							<option value="90 días">90 días</option>
-							<option value="Anticipo 50% y 50% a la entrega">
-								Anticipo 50% y 50% a la entrega
-							</option>
-							<option value="Otra">Otra</option>
-						</select>
-					</div>
-					<div className="flex flex-col items-center gap-4 md:col-span-2">
-						<div className="flex items-center gap-2 justify-start w-full">
-							<FaFileSignature className="text-primary-dark text-2xl" />
-							<label className="text-base">Notas</label>
-						</div>
-						<textarea
-							name="notes"
-							value={form.notes}
-							onChange={(e) => setForm({ ...form, notes: e.target.value })}
-							rows={8}
-							className={`p-6 input w-full rounded-2xl border-2 border-blue-800 ${
-								form.notes
-									? "bg-blue-50 text-primary-dark dark:bg-gray-700 dark:text-gray-300"
-									: ""
-							}`}
-						/>
-					</div>
-				</div>
-				<div ref={contactSectionRef} className="my-20 flex flex-col gap-8">
-					<H4>Datos de Contacto</H4>
-					<div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+				{/* DATOS DE CONTACTO */}
+
+				<div
+					ref={contactSectionRef}
+					className="my-20 flex flex-col gap-8 mt-20">
+					<H4 align="center">Datos de Contacto</H4>
+					<div className="grid grid-cols-1 md:grid-cols-4 gap-8 mt-12">
 						<div className="flex flex-col items-center gap-4">
 							<div className="flex items-center gap-2 justify-start w-full">
 								<FaUser className="text-primary-dark text-2xl" />
@@ -805,6 +807,7 @@ const ClientForm = React.forwardRef(function ClientForm(
 						</div>
 					</div>
 				</div>
+
 				{/* Botones de acción al final del formulario */}
 				<div className="flex flex-col md:flex-row justify-end gap-8 w-full mt-8">
 					<Button
@@ -864,6 +867,7 @@ const ClientForm = React.forwardRef(function ClientForm(
 						size="lg"
 					/>
 				</div>
+
 				{/* Mensajes visuales de éxito o error */}
 				<div className="mt-20 flex flex-col items-end">
 					{message && (
